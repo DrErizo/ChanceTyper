bb2d22abe7d8e4f2ad33dc91b54d14bc8a1589c0
DrErizo <78590012+DrErizo@users.noreply.github.com>
2022-06-07 21:50:29 +0200

    minor changes

--- main.cc
+++ main.cc
@@ -1,23 +1,21 @@
 #include<iostream>
 #include<string.h>
-#include<ctime>
 
 std::string word;
-std::string abc= " !#$%&\'()*+,-./0123456789:;<=>?@ABCDEFGHIJKLMNOPQRSTUVWXYZ[\\]^_`abcdefghijklmnopqrstuvwxyz{|}~";
 int main(int argc, const char **argv) {
 	srand(time(0));
         std::cout << "Input:";
         std::cin >> word;
-        int rng, j;
-        for(int i=0;i<word.length();){
-                rng = rand() % abc.length();
-                if(abc[rng] == word[i]){
-			std::cout << "Found: " << word[i] << "\n";
+        uint64_t j = 0;
+	uint8_t rng;
+        for(uint8_t i=0;i<word.length();){
+                rng = rand() % 255 - 32;
+                if((char)rng == word[i]){
         		i++;
                 }else{
         		i = 0;
         	}
         	j++;	
-		std::cout << "Number of tries: " << j << "\n";
         }
+	std::cout << "Found phrase after: " << j << " tries\n";
 }
